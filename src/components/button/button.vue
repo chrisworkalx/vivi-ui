1e8a7327426637616c977f5b4336846c7eeca122
chrisworkalx <857717575@qq.com>
2022-11-08 17:27:12 +0800

    init-repo

---
+++ src/components/button/button.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  methods: {
+    onClick() {
+      alert('11');
+    },
+    handleClick() {
+      console.log('hello--my-button');
+    }
+  },
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>保存</button>
+      </div>
+    );
+  }
+};
+</script>
+
+<style>
+button {
+  color: blue;
+}
+</style>
